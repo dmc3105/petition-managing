78e9d28eba79050ffb976fc41730e2ee44a79bab
Говоров А.В <algovor04@gmail.com>
2024-09-26 17:51:16 +0700

    Add User and UserRepository

---
+++ backend/src/main/java/ru/dmc3105/petitionmanaging/model/User.java
@@ -0,0 +1,47 @@
+package ru.dmc3105.petitionmanaging.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+}
