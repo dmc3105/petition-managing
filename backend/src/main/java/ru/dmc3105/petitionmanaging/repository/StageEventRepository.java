9068aaf0d0efcd73a9d3718664045f7d1160401f
Govorov Alexey <algovor04@gmail.com>
2024-09-30 22:53:59 +0700

    Update StageEventRepository
    - Add findAllByPetition method
    - Remove @Transactional annotation

--- backend/src/main/java/ru/dmc3105/petitionmanaging/repository/StageEventRepository.java
+++ backend/src/main/java/ru/dmc3105/petitionmanaging/repository/StageEventRepository.java
@@ -1,14 +1,14 @@
 package ru.dmc3105.petitionmanaging.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
+import ru.dmc3105.petitionmanaging.model.Petition;
 import ru.dmc3105.petitionmanaging.model.StageEvent;
 import ru.dmc3105.petitionmanaging.model.User;
 
-import java.util.List;
 import java.util.stream.Stream;
 
 public interface StageEventRepository extends JpaRepository<StageEvent, Long> {
-    @Transactional(readOnly = true)
     Stream<StageEvent> findAllByAssignee(User assignee);
+
+    Stream<StageEvent> findAllByPetition(Petition petition);
 }
