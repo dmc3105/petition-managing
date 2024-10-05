76ea7049bd1a57470fdcc5c810243bc14334e881
Govorov Alexey <algovor04@gmail.com>
2024-10-06 01:12:22 +0700

    Delete duplicates from PetitionAdminController

--- backend/src/main/java/ru/dmc3105/petitionmanaging/controller/PetitionAdminController.java
+++ backend/src/main/java/ru/dmc3105/petitionmanaging/controller/PetitionAdminController.java
@@ -1,15 +1,10 @@
 package ru.dmc3105.petitionmanaging.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.dmc3105.petitionmanaging.controller.request.UpdatePetitionRequest;
-import ru.dmc3105.petitionmanaging.dto.PetitionDto;
 import ru.dmc3105.petitionmanaging.dto.StageEventDto;
 import ru.dmc3105.petitionmanaging.model.Petition;
 import ru.dmc3105.petitionmanaging.service.PetitionService;
@@ -23,14 +18,7 @@ import java.util.List;
 public class PetitionAdminController {
     private PetitionService petitionService;
     private StageEventService stageEventService;
-    private PetitionToDtoMapper mapper;
     private StageEventToDtoMapper stageEventMapper;
-
-    @GetMapping("/{id}")
-    public PetitionDto getPetitionById(@PathVariable Long id) {
-        final Petition petition = petitionService.getPetitionById(id);
-        return mapper.petitionToDto(petition);
-    }
 
     @GetMapping("/{id}/event")
     public List<StageEventDto> getStageEventsByPetitionId(@PathVariable Long id) {
@@ -38,17 +26,5 @@ public class PetitionAdminController {
         return stageEventService.getAllByPetition(petition)
                 .map(event -> stageEventMapper.stageEventToDto(event))
                 .toList();
-    }
-
-    @DeleteMapping("/{id}/delete")
-    public void deletePetitionById(@PathVariable Long id) {
-        petitionService.deletePetition(id);
-    }
-
-    @PutMapping("/{id}/update")
-    public PetitionDto updatePetitionById(@PathVariable Long id,
-                                          @RequestBody UpdatePetitionRequest updatePetitionRequestDto) {
-        final Petition updatedPetition = petitionService.updatePetition(id, updatePetitionRequestDto);
-        return mapper.petitionToDto(updatedPetition);
     }
 }
